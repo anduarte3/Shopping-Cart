ed8c85a40b8453b99c03f91e10ff7677f29c9d3a
FADEwt <diamonvhero@gmail.com>
2023-06-28 19:08:50 +0100

    Implementing checkout functionalities

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { Link, Route, Routes, Navigate } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Shop from './components/Shop';
-import Checkout from './components/Checkout'
-import cart from './img/cart.png'
+import Checkout from './components/Checkout';
+import cart from './img/cart.png';
 import './App.css';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className='main-page'>
       <nav className='nav-bar'>
-        <Link to='/' className='nav-link'>Home</Link>
+        <Link to='/Shopping-Cart/' className='nav-link'>Home</Link>
         <Link to='/shop' className='nav-link'>Shop</Link>
         <Link to='/checkout' className='nav-link'>
           <img src={cart} className='cart-icon'/>
